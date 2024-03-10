b34d6629633dbb9196a43aed25df1796382403a8
Swaraj M Rao <143655010+SwarajMundruppadyRao@users.noreply.github.com>
2024-03-09 21:26:54 -0500

    Added Backtracking + c2c and comments to the code

--- dijkstra_SwarajMundruppady_Rao.py
+++ dijkstra_SwarajMundruppady_Rao.py
@@ -12,7 +12,7 @@ import math
 #Create a empty canvas with value as 0
 canvas=np.full((1200, 500), 0)
 
-#Obstacles
+#Obstacle end point/ coordinates
 rectangle1_coordinates=[[100,100],[175,100],[175,500],[100,500]]
 rectangle1_offsetcoordinates=[[95,95],[180,95],[180,500],[95,500]]
 rectangle2_coordinates=[[275,0],[350,0],[350,400],[275,400]]
@@ -55,20 +55,19 @@ for y in range(500):
             canvas[x, y] = 1
         if polygon.contains_point((x, y)):
             canvas[x, y] = 1
-
-#Pygame map generation
+            
+#To convert coordinates to pygame coordinate system
 def coords_pygame(coords, height):
     return (coords[0], height - coords[1])
 
-def rect_pygame(coords, height, obj_height):
-    return (coords[0], height - coords[1] - obj_height)
-
+# Create the map visualization using pygame
 def create_map(visit, backtrack, start, Goal):
     pygame.init()
+    #Screen Size
     size = [1200, 500]
     screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("Visualization")
-    video = vidmaker.Video("Dijkstra_swarajmr.mp4", late_export=True)
+    pygame.display.set_caption("Dijkstra_Swaraj")
+    video = vidmaker.Video("Dijkstra_SwarajMundruppady_Rao.mp4", late_export=True)
 
     done = False
     clock = pygame.time.Clock()
@@ -77,6 +76,8 @@ def create_map(visit, backtrack, start, Goal):
             if event.type == pygame.QUIT:
                 done = True
         screen.fill("white")
+        
+        # Draw obstacles
         pygame.draw.rect(screen, "blue", [95,0, 85, 405], 0)
         pygame.draw.rect(screen, "red",  [100,0, 75, 400], 0)
         pygame.draw.rect(screen, "blue", [270,95,85,405], 0)
@@ -89,6 +90,8 @@ def create_map(visit, backtrack, start, Goal):
         pygame.draw.circle(screen, (255, 255, 0), coords_pygame(start, 500), 1)
         pygame.draw.circle(screen, (255, 255, 0), coords_pygame(Goal, 500), 1)
         n=0
+        
+        #Visualisation of the explored nodes
         for j in visit:
             n+=1
             pygame.draw.circle(screen, (50, 137, 131), coords_pygame(j, 500), 1)
@@ -98,6 +101,7 @@ def create_map(visit, backtrack, start, Goal):
         pygame.draw.circle(screen, (0, 255, 0), coords_pygame(start, 500), -3)
         pygame.draw.circle(screen, (0, 255, 0), coords_pygame(Goal, 500), -3)
         counter=0
+        #Visualisation of the path generated using backtracking
         for i in backtrack:
             pygame.draw.circle(screen, (255, 255, 0), coords_pygame(i, 500), 1)
             if counter%50==0:
@@ -106,28 +110,46 @@ def create_map(visit, backtrack, start, Goal):
         pygame.draw.circle(screen, (255, 255, 0), coords_pygame(start, 500), 1)
         pygame.draw.circle(screen, (255, 255, 0), coords_pygame(Goal, 500), 1)
         done = True
-        
-    pygame.time.wait(10000)
+    pygame.time.wait(5000)
     pygame.quit()
     video.export(verbose=True)
-    
+ 
+ # Function to check if a given coordinate is inside an obstacle   
 def check_obstacles(coordinates):
     if canvas[coordinates[0], coordinates[1]]==1:
         return False
     return True
 
+# Function to get input for start and goal positions
 def input_start(prompt):
     while True:
-        print("Enter", prompt, "node (x between 0 and 1499, y between 0 and 499) (Sample Input: 10,10): ")
+        print("Enter", prompt, "node (x between 5 and 1194, y between 5 and 494) (Sample Input: 10,10): ")
         input_str = input()
         A = [int(i) for i in input_str.split(',')]
         A_1 = (A[0], A[1])
-        if not (0 <= A[0] < 1500 and 0 <= A[1] < 500):
-            print("Enter valid input (x between 0 and 1499, y between 0 and 499)")
+        if not (0 <= A[0] < 1200 and 0 <= A[1] < 500):
+            print("Enter valid input (x between 5 and 1194, y between 5 and 494)")
         elif not check_obstacles(A_1):
             print("The entered input lies on the obstacles or is not valid, please try again")
         else:
             return A_1
+        
+# Function to check conditions and update the priority queue
+def check_conditions(que, coordinates, cost_to_come):
+    if coordinates in touch:
+        if touch[coordinates] > cost_to_come:
+            new_que = (cost_to_come, coordinates)
+            Path[coordinates] = que[1]
+            Q.put(new_que)
+            touch[coordinates] = cost_to_come
+            return
+        else:
+            return
+    new_que = (cost_to_come, coordinates)
+    Path[coordinates] = que[1]
+    Q.put(new_que)
+    touch[coordinates] = cost_to_come
+
 #Defining Actions and associated cost addition(c2c)
 def moveup(que):
     coordinates = (que[1][0], que[1][1]+1)
@@ -178,6 +200,20 @@ def movedownright(que):
     if check_obstacles(coordinates) and coordinates not in visit:
         cost_to_come = que[0] + 1.4
         check_conditions(que, coordinates, cost_to_come)
+
+## Generate Path using backtracking
+def generate_path(path, start, Goal):
+    backtrack = []
+    key = path.get(Goal)
+    backtrack.append(Goal)
+    backtrack.append(key)
+    while (key != start):
+        key = path.get(key)
+        backtrack.append(key)
+    backtrack.reverse()
+    return backtrack
+
+# Dijkstra Algorithm implementation
 def dijkstra_algorithm():
     #for calculating time required to run the code
     start_time = time.time()
@@ -219,3 +255,13 @@ def dijkstra_algorithm():
             create_map(visit, Backtrack, Start, goal)
             break
 
+
+Start = input_start('Start Position')
+goal = input_start('Goal Position')
+print(Start, goal)
+visit = OrderedSet()
+touch = {}
+Path = {}
+Q = PriorityQueue()
+Q.put((0, Start))
+dijkstra_algorithm()
